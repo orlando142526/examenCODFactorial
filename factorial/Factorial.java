3b6975d0f867a68c93e5839fb67b8e4b96ec7dae
DANIELCASTELAO\oyagualendara <oyagualendara@daniecastelao.org>
2019-11-29 13:59:27 +0100

    refactorizacion

--- factorial/Factorial.java
+++ factorial/Factorial.java
@@ -3,24 +3,22 @@ package factorial;
 public class Factorial {
 
     public static void main(String[] args) {
+        int valor;
+        int factorial;
 
-        int j;
-        int f;
-
-        j = 8;
+        valor = 8;
 
         int i;
-        if (j == 0) {
-            f = 1;
+        if (valor== 0) {
+            factorial = 1;
         } else {
-            f = 1;
-            for (i = j; i >= 1; i--) {
-                f = f * i;
+            factorial = 1;
+            for (i = valor; i >= 1; i--) {
+                factorial = factorial * i;
             }
         }
 
-        System.out.println(f);
-
+        System.out.println(factorial);
     }
-
 }
+// refactorizacion en rename le cambiamos el nombre j=valor , f=factoriaal  
